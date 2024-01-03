21ef9716b23dfd0ad2ea78c7a206bb7109c6b70d
cr180 <cr180@cr180.com>
2024-01-03 15:49:37 +0800

    重做多线程的内部逻辑，让代码逻辑更清晰

--- FastCamera.pyx
+++ FastCamera.pyx
@@ -4,7 +4,6 @@ Email: cr180@cr180.com
 https://github.com/SuperKsa/FastCamera
 LastUpdate: 2024-01-01 00:00:00
 """
-import math
 import platform
 import threading
 import time
@@ -12,8 +11,6 @@ import traceback
 
 import cv2
 import numpy as np
-
-
 
 cdef class FastCamera:
     cdef:
@@ -27,15 +24,15 @@ cdef class FastCamera:
         int Width
         int Height
         int buffSize
-        bint mjpg
+        bint MJPG
         object callback
+        object CallBackInit
         bint stop_threads
         object cap
         bint Exit
         int ths_count
         int ExitThread
-        int readFPS
-        int read_step_time
+        int FPSTime
         list thread_task_map
         list thread_task_status
         int thread_task_currentID
@@ -43,171 +40,123 @@ cdef class FastCamera:
         double Frame_time
         int Frame_tid
         object LOCK
+        bint Debug
 
-    def __cinit__(self, int CameraID=0, int width=0, int height=0, bint mjpg=False, int queueNum=0, int buffSize=10, int thread_count=1, object callback=None):
+    def __init__(self, int CameraID=0, int Width=0, int Height=0, int FPSTime=2, bint MJPG=True, int BuffSize=10, int ThreadCount=1, object Callback=None, object CallBackInit=None, bint Debug=False):
         """
-        初始化
+        初始化参数
         :param CameraID: 相机ID 数字 自动适配linux
-        :param width: 画面 宽度
-        :param height: 画面 高度
-        :param mjpg: 是否启用MJPG
-        :param queueNum: 最大队列数量
-        :param buffSize: 缓存大小
-        :param callback: 回调函数 新帧回调到该函数，参数：(time时间戳, cv2图片对象) 使用回调函数后，不应该再调用read函数，回调函数返回True表示结束进程
-        :param thread_count: 线程数量 高分辨率 高FPS摄像头 建议2-5
+        :param Width: 画面 宽度
+        :param Height: 画面 高度
+        :param FPSTime: 帧采集间隔时间（单位：ms，建议：1-10）
+        :param MJPG: 是否启用MJPG （默认：True）
+        :param BuffSize: OpenCV缓存大小 默认10
+        :param ThreadCount: 需要启用的线程数量(默认：1)
+        :param Callback: 回调函数 新帧回调到该函数，参数：(关联线程ID, time时间戳, cv2图片对象)
+        :param CallBackInit: FastCamera初始化后的回调函数 ，参数：(self)
+        :param Debug: 是否输出debug日志
         """
-
-        # 打印摄像头数量
-
         self.CameraID = CameraID
-        self.Width = width
-        self.Height = height
-        self.mjpg = mjpg
-        self.callback = callback
-        self.buffSize = buffSize
-        self.ths_count = thread_count
+        self.Width = Width
+        self.Height = Height
+        self.FPSTime = FPSTime
+        self.MJPG = MJPG
+        self.buffSize = BuffSize
+        if ThreadCount > 0:
+            self.ths_count = ThreadCount
+        else:
+            self.ths_count = 1
+
+        self.callback = Callback
+        self.CallBackInit = CallBackInit  # 启动完成的回调函数
+        self.Debug = Debug
+
         self.cap = None
-
-        self.readFPS = 60  # 采集帧率
-        self.read_step_time = math.floor(1000 / self.readFPS) - 10  # 每帧间隔时间
-        self.thread_task_map = []
-        self.thread_task_status = []
+        self.thread_task_map = []  # 每个线程的任务ID key=线程ID value=任务ID
+        self.thread_task_status = []  # 每个线程的运行状态 key=线程ID value=状态值(0=空闲 1=忙碌)
         self.thread_task_currentID = 0  # 当前任务线程ID
-        self.Frame_Data = np.zeros((height, width, 3), dtype=np.uint8)
-        self.Frame_time = 0
+        self.Frame_Data = np.zeros((Height, Width, 3), dtype=np.uint8)  # 当前帧cv2数据
+        self.Frame_time = 0  # 当前帧时间戳
 
-        self.LOCK = threading.Lock()
+        self.LOCK = threading.Lock()  # 线程锁
+        self.Exit = False  # 当前进程退出信号
 
+        self._start()
 
+        # 全局控制线程
+        th_ctrl = threading.Thread(target=self._ThreadFunc_Ctrl)
+        th_ctrl.daemon = True  #子线程必须和主进程一同退出，防止僵尸进程
+        th_ctrl.start()
 
         # 启动帧回调进程
-        th = threading.Thread(target=self._callFunc)
-        th.daemon = True  #子线程必须和主进程一同退出，防止僵尸进程
-        th.start()
+        th_call = threading.Thread(target=self._ThreadFunc__callFunc)
+        th_call.daemon = True  #子线程必须和主进程一同退出，防止僵尸进程
+        th_call.start()
 
-        self.start()
+        # 初始化完成后回调给外部
+        if CallBackInit is not None:
+            CallBackInit(self)
 
+    def _start(self):
 
-        self.Exit = False
+        debugString = '\n'
+        debugString += ('=' * 20)
+        debugString += f' FastCamera '
+        debugString += ('=' * 20)
 
-        loopN = 0
-        while True:
-            self.read_step_time = math.floor(1000 / self.readFPS) - 10
-            if self.Exit:
-                while True:
-                    if self.ExitThread == self.ths_count:
-                        return
-                    time.sleep(0.001)
-
-            elif self.Status == 1:
-                print('【摄像头重启中】')
-                self.release(False)
-                while self.ExitThread != self.ths_count:
-                    time.sleep(0.001)
-                self.start()
-                print('【摄像头重启完成】')
-                self.startTime = time.time()
-                self.FPS_count = 0
-
-            try:
-                if loopN >= self.read_step_time:
-                    loopN = 0
-                    # 查找空闲线程 并分配任务
-                    for i in range(self.ths_count):
-                        # 空闲线程 status=0 分配任务hash=时间戳
-                        if self.thread_task_status[i] == 0:
-                            self.thread_task_currentID = i
-                            # print(f'分配任务给 {i}')
-                            taskID = time.time()
-                            self.thread_task_map[i] = taskID
-                            break
-            except Exception as e:
-                print('任务分配主进程报错：', e)
-                pass
-            loopN += 1
-
-            time.sleep(0.001)  # 1ms间隔
-
-        print('【VideoCapture】进程退出')
-        # sys.exit()
-
-    def restart(self, CameraID=0, width=0, height=0, mjpg=False, queueNum=0, buffSize=10):
-        """
-        重启摄像头
-        :return:
-        """
-        self.CameraID = CameraID
-        self.Width = width
-        self.Height = height
-        self.mjpg = mjpg
-
-
-        self.Status = 1
-
-
-    def start(self):
         self.ExitThread = 0  # 当前结束线程数量
         self.stop_threads = False  # 当前进程是否停止
-        self.Status = 0
+        self.Status = 0  # 标记状态为初始
 
         self.thread_task_status = []
         self.thread_task_map = []
+
         for i in range(self.ths_count):
             self.thread_task_status.append(0)  # 标记线程状态为空闲
             self.thread_task_map.append(0)  # 标记线程任务为空
-
-            th = threading.Thread(target=self._reader, args=(i,))
+            th = threading.Thread(target=self.__ThreadFunc_reader, args=(i,))
             th.daemon = True  #子线程必须和主进程一同退出，防止僵尸进程
             th.start()
-
+            debugString += (f'\n= Read Thread-{i} Start')
 
         if platform.system().lower() == 'linux':
-            print('=' * 40)
-            print(f'= 摄像头初始化中 ID=/dev/video{self.CameraID} ThreadCount={self.ths_count}')
+            debugString += (f'\n= 摄像头初始化中 ID=/dev/video{self.CameraID} ThreadCount={self.ths_count}')
             self.cap = cv2.VideoCapture(f'/dev/video{self.CameraID}')
         else:
-            print('=' * 40)
-            print(f'= 摄像头初始化中 ID={self.CameraID} ThreadCount={self.ths_count}')
+            debugString += (f'\n= 摄像头初始化中 ID={self.CameraID} ThreadCount={self.ths_count}')
             self.cap = cv2.VideoCapture(self.CameraID)
         # 检查摄像头是否成功打开
         if self.cap.isOpened():
-            print(f'= 摄像头已打开')
+            debugString += (f'\n= 摄像头已打开')
 
             if self.buffSize > 0:
                 self.cap.set(cv2.CAP_PROP_BUFFERSIZE, self.buffSize)
-            if self.mjpg:
-                self.set_mjpg()
-            self.set_size()
+            if self.MJPG:
+                res_mjpg = self.set_mjpg()
+                debugString += (f'\n= MJPG设置结果={res_mjpg}')
+                res_mjpg = None
+
+            res_size = self.set_size()
+            debugString += (f'\n= 尺寸={self.Width}x{self.Height} 设置结果={res_size}')
+            res_size = None
 
             self.startTime = time.time()
-            self.Status = 2
+            self.Status = 2  # 标记状态为启动成功
 
-            print('= 摄像头初始化完成')
+            debugString += ('\n= 摄像头初始化完成')
         else:
-            print(f'= 摄像头打开失败 {self.CameraID}')
-        print('=' * 40)
+            debugString += (f'\n= 摄像头打开失败 {self.CameraID}')
 
-    def set_mjpg(self):
-        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('m', 'j', 'p', 'g'))
-        setMJPG = self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-        print(f'= MJPG设置结果={setMJPG}')
-        return setMJPG
+        if self.Debug:
+            debugString += (f'\n= 线程数量={self.ths_count}')
+            debugString += (f'\n= OpenCV BuffSize={self.buffSize}')
+            debugString += (f'\n= FPSTime={self.FPSTime}')
+            debugString += '\n'
+            debugString += ('=' * 50)
+            debugString += '\n'
+            print(debugString)
 
-    def set_size(self, int width=0, int height=0):
-        if width > 0:
-            self.Width = width
-        if height > 0:
-            self.Height = height
-
-        if self.Width > 0 and self.Height > 0:
-            setW = self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.Width)  # 设置帧宽度
-            setH = self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.Height)  # 设置帧高度
-            print(f'= 尺寸={self.Width}x{self.Height} 设置结果={setW and setH}')
-
-    def set(self, int key, int value):
-        self.cap.set(key, value)
-
-    def addFrame(self, taskID, tid, times, frame):
+    def _addFrame(self, taskID, tid, times, frame):
         with self.LOCK:
             # t = time.time()
             try:
@@ -218,46 +167,46 @@ cdef class FastCamera:
                     self.Frame_time = times
                     self.Frame_tid = tid
                     # print(f'更新帧耗时={round((time.time() - t) * 1000)}ms')
-            except AttributeError as e:
-                print(f"图像不完整1: {e}")
-            except IOError as e:
-                print(f"发生IOError2: {e}")
             except Exception as e:
-                print(f"发生了其他异常: {e}")
+                if self.Debug:
+                    print(f"[FastCamera] 发生异常: {e}")
 
-    def _callFunc(self):
+    def _ThreadFunc__callFunc(self):
         """
         帧回调函数 将帧回调给外部调用函数
         :return:
         """
         sendTime = 0.
         while True:
-            if self.stop_threads:
-                print(f'【FastCamera】 回调进程退出 OutEd')
+            if self.Exit:
+                if self.Debug:
+                    print(f'[FastCamera] 回调进程 退出')
                 break
-            if sendTime < self.Frame_time:
-                try:
-                    shape = self.Frame_Data.shape
-                    # t = time.time()
-                    self.callback(self, self.Frame_tid, self.Frame_time, self.Frame_Data)
-                    # print(f'推送帧耗时={round((time.time() - t) * 1000)}ms')
-                except AttributeError as e:
-                    print(f"图像不完整: {e}")
-                except IOError as e:
-                    print(f"发生IOError: {e}")
-                except Exception as e:
-                    print('帧回调函数报错：', e)
-                    pass
-                sendTime = self.Frame_time
+            if self.callback is not None:
+                if sendTime < self.Frame_time:
+                    try:
+                        shape = self.Frame_Data.shape
+                        # t = time.time()
+                        self.callback(self.Frame_tid, self.Frame_time, self.Frame_Data)
+                        # print(f'推送帧耗时={round((time.time() - t) * 1000)}ms')
+                    except Exception as e:
+                        if self.Debug:
+                            print('[FastCamera] 帧回调函数报错：', e)
+                        pass
+                    sendTime = self.Frame_time
             time.sleep(0.001)
+        self.ExitThread += 1
 
-
-    # 实时读帧，将所有帧保存到队列
-    def _reader(self, tid):
-        print(f'【FastCamera】 Thread-{tid} 启动')
+    def __ThreadFunc_reader(self, tid):
+        """
+        线程执行函数
+        :param tid:
+        :return:
+        """
         while True:
             if self.stop_threads:
-                print(f'【FastCamera】 Thread-{tid} OutEd')
+                if self.Debug:
+                    print(f'[FastCamera] Thread-{tid} OutEd')
                 break
             # 如果当前存在任务
             taskID = self.thread_task_map[tid]
@@ -272,7 +221,7 @@ cdef class FastCamera:
                     if ret:
                         self.Status = 3
                         frame_time = startFrameTime
-                        self.addFrame(taskID, tid, frame_time, frame)  # 画面添加到帧
+                        self._addFrame(taskID, tid, frame_time, frame)  # 画面添加到帧
 
                         # -------- FPS统计处理 Start -------- #
                         time_x = (time.time() - self.startTime)
@@ -289,7 +238,8 @@ cdef class FastCamera:
 
                 except Exception as e:
                     traceback.print_exc()
-                    print('帧读取线程报错：', e)
+                    if self.Debug:
+                        print('[FastCamera] 帧读取线程报错：', e)
                     pass
                 self.thread_task_status[tid] = 0  # 标记当前进程为 空闲
                 self.thread_task_map[tid] = 0
@@ -298,6 +248,86 @@ cdef class FastCamera:
 
         self.ExitThread += 1
 
+    def _ThreadFunc_Ctrl(self):
+        """
+        线程函数 - 全局控制
+        :return:
+        """
+        loopN = 0
+        while True:
+            if self.Exit:
+                if self.Debug:
+                    print('[FastCamera] 控制线程 退出')
+                break
+
+            elif self.Status == 1:
+                if self.Debug:
+                    print('[FastCamera] 摄像头重启中')
+                self.release(False)
+                while self.ExitThread != self.ths_count:
+                    time.sleep(0.001)
+                self._start()
+                if self.Debug:
+                    print('[FastCamera] 摄像头重启完成')
+                self.startTime = time.time()
+                self.FPS_count = 0
+
+            try:
+                if loopN >= self.FPSTime:
+                    loopN = 0
+                    # 查找空闲线程 并分配任务
+                    for i in range(self.ths_count):
+                        # 空闲线程 status=0 分配任务hash=时间戳
+                        if self.thread_task_status[i] == 0:
+                            self.thread_task_currentID = i
+
+                            taskID = time.time()
+                            # print(f'{self.FPSTime}ms 分配任务给 {i} = {taskID}')
+                            self.thread_task_map[i] = taskID
+                            break
+            except Exception as e:
+                if self.Debug:
+                    print('[FastCamera] 任务分配主进程报错：', e)
+                pass
+            loopN += 1
+
+            time.sleep(0.001)  # 1ms间隔
+        self.ExitThread += 1
+
+    def restart(self, int CameraID=-1, int Width=0, int Height=0, int FPSTime=0, bint MJPG=True, int BuffSize=-1, int ThreadCount=-1):
+        """
+        重启摄像头
+        :return:
+        """
+        if CameraID > 0 and Width > 0 and Height > 0:
+            self.CameraID = CameraID
+            self.Width = Width
+            self.Height = Height
+            self.FPSTime = FPSTime
+            self.MJPG = MJPG
+            self.buffSize = BuffSize
+            self.ths_count = ThreadCount
+        self.Status = 1  # 标记为重启信号
+
+    def set_mjpg(self):
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('m', 'j', 'p', 'g'))
+        setMJPG = self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        return setMJPG
+
+    def set_size(self, int width=0, int height=0):
+        if width > 0:
+            self.Width = width
+        if height > 0:
+            self.Height = height
+
+        if self.Width > 0 and self.Height > 0:
+            setW = self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.Width)  # 设置帧宽度
+            setH = self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.Height)  # 设置帧高度
+            return setW and setH
+        return False
+
+    def set(self, int key, int value):
+        self.cap.set(key, value)
 
     def fps(self) -> float:
         """
@@ -322,14 +352,36 @@ cdef class FastCamera:
             return round(self.readTime * 1000 / self.ths_count)
         return 0
 
+    def read(self):
+        """
+        读取当前实时帧数据
+        :return:
+        """
+        return self.Frame_time, self.Frame_Data
+
     def release(self, isExit=True):
         """
         关闭摄像头
+        :param isExit: 是否结束整个进程
         :return:
         """
         self.stop_threads = True
         if self.cap is not None:
             self.cap.release()
+
+        # 退出程序
         if isExit:
             self.Exit = True
+            thCount = self.ths_count + 2
+            while True:
+                if self.ExitThread == thCount:
+                    break
+                time.sleep(0.001)
+            # 清理数据
+            self.Frame_Data = None
+            self.Frame_time = 0
+            self.thread_task_status = []
+            self.thread_task_map = []
+            if self.Debug:
+                print('[FastCamera] Work End ExitEd')
         return True
--- demo.py
+++ demo.py
@@ -4,7 +4,6 @@ Email: cr180@cr180.com
 https://github.com/SuperKsa/FastCamera
 LastUpdate: 2024-01-01 00:00:00
 """
-
 import signal
 import sys
 import time
@@ -15,80 +14,110 @@ import cv2
 
 from FastCamera import FastCamera
 
-MainExit = False
 
-# ---------- 监听主进程退出 ---------- #
-def signal_handler(sig, frame):
-    global MainExit
-    MainExit = True
-    print('Detecting Ctrl+C to exit the main process')
-signal.signal(signal.SIGINT, signal_handler)
-# ---------- 监听主进程退出 ---------- #
+class Demo:
+    MainExit = False
+    # ---------- Config ---------- #
+    Config_Width = 2688  # camera Width 分辨率-宽
+    Config_Height = 1520  # camera Height  分辨率-高
+    Config_FPSTime = 1  # FPS
+    Config_MJPG = True  # is MJPG 你应该启用MJPG
+    Config_CameraID = 0  # camera ID  摄像头ID
+    Config_buffSize = 10  # opencv buffer
+    Config_ThreadCount = 4  # Number of thread 启用多少个子线程(>2K 60FPS 应该设置2-5)
+    # ---------- Config ---------- #
 
+    # ---------- 统计FPS 常量 ---------- #
+    startTime = time.time()
+    FPS_count = 0
+    FPS = 0
+    Frame_Data = None
+    Frame_Time = 0
+    Frame_Tid = 0
+    # ---------- 统计FPS 常量 ---------- #
 
-# ---------- Config ---------- #
-Width = 2688  # camera Width 分辨率-宽
-Height = 1520  # camera Height  分辨率-高
-MJPG = True  # is MJPG 你应该启用MJPG
-CameraID = 0  # camera ID  摄像头ID
-buffSize = 3  # opencv buffer
-CapThreadCount = 4  # Number of thread 启用多少个子线程(>2K 60FPS 应该设置2-5)
-QueueNum = 1000  # read Queue num  （帧队列 待启用的参数）
+    def __init__(self):
+        # 监听Ctrl+C信号
+        signal.signal(signal.SIGINT, self.signal_handler)
 
-CameraParam = {
-    'Width' : 2688,  # camera Width 分辨率-宽
-    'Height' : 1520,  # camera Height  分辨率-高
-    'MJPG' : True,  # is MJPG 你应该启用MJPG
-    'CameraID' : 20,  # camera ID  摄像头ID
-    'BuffSize' : 0,  # opencv buffer
-}
-# ---------- Config ---------- #
+        # 启动 FastCamera
+        self.cap = FastCamera(CameraID=self.Config_CameraID, Width=self.Config_Width, Height=self.Config_Height, MJPG=self.Config_MJPG, FPSTime=self.Config_FPSTime, BuffSize=self.Config_buffSize, ThreadCount=self.Config_ThreadCount, Callback=self.callbackFrame, CallBackInit=self.callbackInit, Debug=True)
 
-def callbackFrame(cap:FastCamera, times, frame):
-    """
-    This is a callback function that receives the latest frame image data.
-    这是一个回调函数，接收最新的帧画面数据
-    :param cap: cap Class Object (FastCamera类对象)
-    :param times:  Current Frame Key (当前帧键名 time.time时间戳)
-    :param frame:  Current frame (opencv img object) 当前帧opencv图片对象
-    :return: If it returns True, FastCamera will terminate its operation.（如果返回True FastCamera将会结束运行）
-    """
+        # ---------- Detecting Main Process Exit ---------- #
+        while True:
+            # 监听到进程退出信号
+            if self.MainExit:
+                print('开始关闭进程')
+                self.cap.release()
+                break
 
-    # ---------- Detecting Main Process Exit ---------- #
-    global MainExit
-    if MainExit:
-        cap.release()  # exit FastCamera class  (主动结束)
-        return True  # or return True exit （可选择的）
-    # ---------- Detecting Main Process Exit ---------- #
+            # 显示实时帧(cv2.imshow会消耗CPU资源，同时导致整个采集工作变慢！)
+            if self.Frame_Data is not None:
+                cv2.imshow("FastCamera Frame", self.Frame_Data)
 
+            debugString = ''
 
-    # 这里仅作为demo例子，实际过程中，你应该在收到frame数据以后立即存入队列中，而不应该在这里进行耗时逻辑！！ #
-    # This is only a demo example. In actual implementation, you should immediately store the received frame data in a queue, and avoid time-consuming logic in this context. #
+            # 提取实时帧形状与第一个像素值
+            if self.Frame_Data is not None:
+                debugString = f'frame={self.Frame_Data.shape} {self.Frame_Data[0:1, 0:1, :]}'
 
-    debugString = ''
-    if times > 0:
-        debugString = f'frame={frame.shape}'
-        cv2.imshow("FastCamera Frame", frame)
+            # 输出统计信息
+            CurrentDate = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S.%f")
+            sys.stdout.write(f'\r[{CurrentDate}] FPS={self.FPS}/{self.cap.fps()} FrameTime={self.cap.read_time()}ms/{self.cap.fps_time()}ms FrameKey={self.Frame_Time} 当前任务ID={self.Frame_Tid} MainExit={self.MainExit} {debugString}')
+            sys.stdout.flush()
 
+            # 监听cv2窗口键鼠信号
+            waitKey = cv2.waitKey(1)
+            if waitKey == 27:  # ESC 退出程序
+                print('收到退出信号')
+                self.MainExit = True
+            elif waitKey == ord('r'):  # R键 重启摄像头 Restart
+                # 方式 1：重启时传入新的参数
+                # self.cap.restart(CameraID=self.Config_CameraID, Width=self.Config_Width, Height=self.Config_Height, MJPG=self.Config_MJPG, FPSTime=self.Config_FPSTime, BuffSize=self.Config_buffSize, ThreadCount=self.Config_ThreadCount)
 
-    CurrentDate = datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S.%f")
-    sys.stdout.write(f'\r[{CurrentDate}] FPS={cap.fps()} FrameTime={cap.read_time()}ms/{cap.fps_time()}ms FrameKey={times} MainExit={MainExit} {debugString}')
-    sys.stdout.flush()
+                # 方式 2：使用初始化参数直接重启
+                self.cap.restart()
 
-    waitKey = cv2.waitKey(1)
-    if waitKey == 27:
-        cap.release()
-    elif waitKey == ord('q'):  # Exit
-        cap.release()
+            # time.sleep(0.001)
+        cv2.destroyAllWindows()
+        # ---------- Detecting Main Process Exit ---------- #
 
-    elif waitKey == ord('r'):  # Restart
-        cap.restart(CameraID=CameraID, width=Width, height=Height, mjpg=MJPG, queueNum=QueueNum, buffSize=buffSize)
+    def callbackInit(self, fc):
+        print('FastCamera初始化完成')
 
+    def callbackFrame(self, tid, times, frame):
+        """
+        This is a callback function that receives the latest frame image data.
+        这是一个回调函数，接收最新的帧画面数据
+        :param cap: cap Class Object (FastCamera类对象)
+        :param times:  Current Frame Key (当前帧键名 time.time时间戳)
+        :param frame:  Current frame (opencv img object) 当前帧opencv图片对象
+        :return: If it returns True, FastCamera will terminate its operation.（如果返回True FastCamera将会结束运行）
+        """
+
+        # 这里仅作为demo例子，实际过程中，你应该在收到frame数据以后立即存入队列中，而不应该在这里进行耗时逻辑！！ #
+        # This is only a demo example. In actual implementation, you should immediately store the received frame data in a queue, and avoid time-consuming logic in this context. #
+
+        # 将帧数据传递到 self.Frame_Data
+        if times > 0:
+            self.Frame_Data = frame  # 当前帧
+            self.Frame_Time = times  # 当前帧对应时间戳
+            self.Frame_Tid = tid  # 读取当前帧的线程ID
+
+            # 计算FPS
+            self.FPS_count += 1
+            self.FPS = round(self.FPS_count / (time.time() - self.startTime), 1)
+
+    def signal_handler(self, sig, frame):
+        self.MainExit = True
 
 
 if __name__ == '__main__':
-    P_CapRead = Process(name='FastCamera', target=FastCamera, args=(CameraID, Width, Height, MJPG, QueueNum, buffSize, callbackFrame, CapThreadCount, ))
+
+    # 启动线程
+    P_CapRead = Process(name='DemoProcess', target=Demo)
     P_CapRead.daemon = True
     P_CapRead.start()
-    P_CapRead.join()
+    P_CapRead.join()  #阻塞并等待进程结束
+
     print('Main Exit.')
